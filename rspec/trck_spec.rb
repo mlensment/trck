412c7e41827287305535d732afd1af4f73094edc
Jette <jette@deskrock.ee>
2013-08-13 14:26:43 +0300

    added new status test and project removing without tasks test, corrected older ones

--- rspec/trck_spec.rb
+++ rspec/trck_spec.rb
@@ -41,13 +41,15 @@ it 'handles basic task commands' do
 
 end
 
-it 'handles two simultaneously running tasks' do
+it 'gives information about multiple tasks' do
 
     trck('add task 100').should eq('Task 100 was added')
 
     trck('add task 200').should eq('Task 200 was added')
 
     trck('start 100').should eq('Task 100 was started')
+    trck('start 100').should eq('Task 100 already started')
+
     trck('start 200').should eq('Task 200 was started')
 
     trck('status').should eq("Currently running tasks:\n100 - 0h 0m 0s\n200 - 0h 0m 0s\n")
@@ -82,6 +84,10 @@ it 'handles projects' do
 end
 
 it 'handles tasks in projects' do
+
+    #first project
+    trck('add project chatroom').should eq('Project chatroom was added')
+
     trck('add task chatroom 100').should eq('Task 100 was added to project chatroom')
 
     trck('add task chatroom 100').should eq('Cannot create two tasks with same name')
@@ -98,13 +104,14 @@ it 'handles tasks in projects' do
 
     trck('tasks chatroom').should eq("200 - 0h 0m 0s")
 
+    #other project
+    trck('add project abahn').should eq('Project abahn was added')
+
     trck('add task abahn calendar').should eq('Task calendar was added to project abahn')
 
     trck('add task calendar').should eq('Cannot create two tasks with same name')
 
     trck('start abahn calendar').should eq('Task calendar was started')
-
-    trck('start abahn calendar').should eq('Task calendar already started')
 
     trck('status abahn').should eq("Currently running tasks:\ncalendar - 0h 0m 0s\n") #no functionality?
 
@@ -124,15 +131,47 @@ it 'handles tasks in projects' do
     trck('start calendar').should eq('Task calendar was not found')
 end
 
-it 'removes projects' do
+it 'removes projects with all its tasks' do
 
-    trck('remove project abahn').should eq('Project abahn with all tasks was removed') no functionality
+    trck('add project abahn').should eq('Project abahn was added')
 
-    trck('remove project abahn').should eq('Project abahn was not found')
+    trck('remove project abahn -r').should eq('Project abahn with all tasks was removed') #no functionality
 
-    trck('remove project chatroom').should eq('Project chatroom with all tasks was removed')
+    trck('remove project abahn -r').should eq('Project abahn was not found')
 
     trck('projects').should eq("No projects found")
+
 end
+
+it 'removes project but keeps tasks' do
+
+    trck('add project myproject').should eq('Project myproject was added')
+
+    trck('add task myproject calendar').should eq('Task calendar was added to project myproject')
+
+    trck('remove project myproject').should eq('Project myproject was removed') #no functionality
+    trck('remove project myproject').should eq('Project myproject was not found') #no functionality
+
+    trck('start calendar').should eq('Task calendar was started')
+    trck('stop calendar').should eq('Task calendar was stopped')
+
+end
+
+it 'shows status about multiple projects' do #no functionality
+        trck('add project myproject').should eq('Project myproject was added')
+        trck('add project second_project').should eq('Project second_project was added')
+
+        trck('add task myproject eatlunch').should eq('Task eatlunch was added to project myproject')
+        trck('start myproject eatlunch').should eq('Task eatlunch was started')
+        trck('stop myproject eatlunch').should eq('Task eatlunch was stopped')
+        trck('add task myproject whatever').should eq('Task whatever was added to project myproject')
+
+
+        trck('add task second_project eatdinner').should eq('Task eatdinner was added to project second_project')
+        trck('start second_project eatdinner').should eq('Task eatdinner was started')
+
+        trck('status').should eq("Currently running tasks:\nsecond_project\n\teatdinner - 0h 0m 0s\nLast tracked tasks:\nmyproject\n\teatlunch - 0h 0m 0s\n\twhatever - not started\n")
+end
+
 
 end
